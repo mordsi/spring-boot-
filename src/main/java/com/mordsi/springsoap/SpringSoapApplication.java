c439114458758fe18513e4a62c17d4cdb5b6ab17
mordsi <46667659+mordsi@users.noreply.github.com>
2019-01-14 14:30:10 +0700

    Add files via upload

---
+++ src/main/java/com/mordsi/springsoap/SpringSoapApplication.java
@@ -0,0 +1,14 @@
+package com.mordsi.springsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapApplication.class, args);
+	}
+
+}
+
